61e15226badac63b24a14b842d7e3f827c33e229
dungldt <langdinhthanhdung@gmail.com>
2024-09-04 13:59:57 +0700

    modify projects

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -1,15 +1,14 @@
 <script>
 import feather from 'feather-icons';
-import ProjectsFilter from './ProjectsFilter.vue';
 import ProjectSingle from './ProjectSingle.vue';
 import projects from '../../data/projects';
 
 export default {
-	components: { ProjectSingle, ProjectsFilter },
+	components: { ProjectSingle },
 	data: () => {
 		return {
 			projects,
-			projectsHeading: 'Projects Portfolio',
+			projectsHeading: 'Projects',
 			selectedCategory: '',
 			searchProject: '',
 		};
@@ -60,76 +59,6 @@ export default {
 			</p>
 		</div>
 
-		<!-- Filter and search projects -->
-		<div class="mt-10 sm:mt-10">
-			<h3
-				class="font-general-regular
-					text-center text-secondary-dark
-					dark:text-ternary-light
-					text-md
-					sm:text-xl
-					font-normal
-					mb-4
-				"
-			>
-				Search projects by title or filter by category
-			</h3>
-			<div
-				class="
-					flex
-					justify-between
-					border-b border-primary-light
-					dark:border-secondary-dark
-					pb-3
-					gap-2
-				"
-			>
-				<div class="flex justify-between gap-2">
-					<span
-						class="
-							hidden
-							sm:block
-							bg-primary-light
-							dark:bg-ternary-dark
-							p-2.5
-							shadow-sm
-							rounded-xl
-							cursor-pointer
-							"
-					>
-						<i
-							data-feather="search"
-							class="text-ternary-dark dark:text-ternary-light"
-						></i>
-					</span>
-					<input
-						v-model="searchProject"
-						class="font-general-medium
-						pl-3
-						pr-1
-						sm:px-4
-						py-2
-						border-1 border-gray-200
-						dark:border-secondary-dark
-						rounded-lg
-						text-sm
-						sm:text-md
-						bg-secondary-light
-						dark:bg-ternary-dark
-						text-primary-dark
-						dark:text-ternary-light
-						"
-						id="name"
-						name="name"
-						type="search"
-						required=""
-						placeholder="Search Projects"
-						aria-label="Name"
-					/>
-				</div>
-				<ProjectsFilter @filter="selectedCategory = $event" />
-			</div>
-		</div>
 
 		<!-- Projects grid -->
 		<div
