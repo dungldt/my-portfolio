9d03979afb44a312040f0736fc91e636e93e35b7
dungldt <langdinhthanhdung@gmail.com>
2024-09-04 23:46:10 +0700

    add skills component

---
+++ src/components/about/AboutSkillSingle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: ["skill"],
+};
+</script>
+
+<template>
+    <li
+      className="
+                rounded-xl px-3 py-2
+               bg-black/60 dark:bg-white/10 text-white dark:text-white/80"
+      key="{skill.id}"
+    >
+      {{skill.name}}
+    </li>
+</template>
+
+<style lang="scss" scoped></style>
