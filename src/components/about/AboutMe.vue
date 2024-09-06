1b12cb7b92a2be4919e32096550ec703bacab5de
dungldt <langdinhthanhdung@gmail.com>
2024-09-06 14:32:26 +0700

    center and scale smaller about page

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -1,44 +1,54 @@
 <script>
 export default {
-	setup() {
-		return {
-			bios: [
-				{
-					id: 1,
-					bio:
-						'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
-				},
-				{
-					id: 2,
-					bio:
-						'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-				},
-			],
-		};
-	},
+  setup() {
+    return {
+      bios: [
+        {
+          id: 1,
+          bio: "• More than 11 years of experience in software development",
+        },
+        {
+          id: 2,
+          bio: "• Mainly work as a back-end developer",
+        },
+        {
+          id: 3,
+          bio: "• Be able to work independently or as part of a team",
+        },
+        {
+          id: 4,
+          bio: "• Have experience in website development",
+        },
+        {
+          id: 4,
+          bio: "• Have experience with client-server API interaction",
+        },
+        {
+          id: 5,
+          bio: "• Have the ability of training members and team management",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
-	<div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
-		<!-- About profile image -->
-		<div class="w-full sm:w-1/4 mb-7 sm:mb-0">
-			<img
-				src="@/assets/images/profile.jpeg"
-				class="rounded-xl w-96"
-				alt=""
-			/>
-		</div>
+  <div class="block sm:flex sm:gap-10 mt-10 sm:mt-20 pl-20">
+    <!-- About profile image -->
+    <div class="w-full sm:w-1/4 mb-7 sm:mb-0 grid place-items-center flex-[0.4]">
+      <img src="@/assets/images/profile.jpeg" class="rounded-xl w-60" alt="" />
+    </div>
 
-		<!-- About details -->
-		<div class="w-full sm:w-3/4 text-left">
-			<p
-				v-for="bio in bios"
-				:key="bio.id"
-				class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
-			>
-				{{ bio.bio }}
-			</p>
-		</div>
-	</div>
+    <!-- About details -->
+    <div class="w-full sm:w-3/4 text-left flex-[0.6]">
+      <p
+        v-for="bio in bios"
+        :key="bio.id"
+        class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+      >
+        {{ bio.bio }}
+      </p>
+    </div>
+  </div>
 </template>
