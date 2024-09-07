eb799f5f12d6a67e88249785ad1e7c25ebbd9fd6
BBMCode <bbmcode247@gmail.com>
2024-09-07 10:02:43 +0700

    format project component

--- src/components/projects/ProjectsGrid.vue
+++ src/components/projects/ProjectsGrid.vue
@@ -1,76 +1,72 @@
 <script>
-import feather from 'feather-icons';
-import ProjectSingle from './ProjectSingle.vue';
-import projects from '../../data/projects';
+import feather from "feather-icons";
+import ProjectSingle from "./ProjectSingle.vue";
+import projects from "../../data/projects";
 
 export default {
-	components: { ProjectSingle },
-	data: () => {
-		return {
-			projects,
-			projectsHeading: 'Projects',
-			selectedCategory: '',
-			searchProject: '',
-		};
-	},
-	computed: {
-		// Get the filtered projects
-		filteredProjects() {
-			if (this.selectedCategory) {
-				return this.filterProjectsByCategory();
-			} else if (this.searchProject) {
-				return this.filterProjectsBySearch();
-			}
-			return this.projects;
-		},
-	},
-	methods: {
-		// Filter projects by category
-		filterProjectsByCategory() {
-			return this.projects.filter((item) => {
-				let category =
-					item.category.charAt(0).toUpperCase() +
-					item.category.slice(1);
-				console.log(category);
-				return category.includes(this.selectedCategory);
-			});
-		},
-		// Filter projects by title search
-		filterProjectsBySearch() {
-			let project = new RegExp(this.searchProject, 'i');
-			return this.projects.filter((el) => el.title.match(project));
-		},
-	},
-	mounted() {
-		feather.replace();
-	},
+  components: { ProjectSingle },
+  data: () => {
+    return {
+      projects,
+      projectsHeading: "Projects",
+      selectedCategory: "",
+      searchProject: "",
+    };
+  },
+  computed: {
+    // Get the filtered projects
+    filteredProjects() {
+      if (this.selectedCategory) {
+        return this.filterProjectsByCategory();
+      } else if (this.searchProject) {
+        return this.filterProjectsBySearch();
+      }
+      return this.projects;
+    },
+  },
+  methods: {
+    // Filter projects by category
+    filterProjectsByCategory() {
+      return this.projects.filter((item) => {
+        let category =
+          item.category.charAt(0).toUpperCase() + item.category.slice(1);
+        console.log(category);
+        return category.includes(this.selectedCategory);
+      });
+    },
+    // Filter projects by title search
+    filterProjectsBySearch() {
+      let project = new RegExp(this.searchProject, "i");
+      return this.projects.filter((el) => el.title.match(project));
+    },
+  },
+  mounted() {
+    feather.replace();
+  },
 };
 </script>
 
 <template>
-	<!-- Projects grid -->
-	<section class="pt-10 sm:pt-14">
-		<!-- Projects grid title -->
-		<div class="text-center">
-			<p
-				class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light"
-			>
-				{{ projectsHeading }}
-			</p>
-		</div>
+  <!-- Projects grid -->
+  <section class="pt-10 sm:pt-14">
+    <!-- Projects grid title -->
+    <div class="text-center">
+      <p
+        class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light"
+      >
+        {{ projectsHeading }}
+      </p>
+    </div>
 
-
-		<!-- Projects grid -->
-		<div
-			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10"
-		>
-			<ProjectSingle
-				v-for="project in filteredProjects"
-				:key="project.id"
-				:project="project"
-			/>
-		</div>
-	</section>
+    <!-- Projects grid -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
+      <ProjectSingle
+        v-for="project in filteredProjects"
+        :key="project.id"
+        :project="project"
+      />
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
