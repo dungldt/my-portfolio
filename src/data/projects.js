a41767d75e39e7c244a2e7fd378dc48c103a03be
BBMCode <bbmcode247@gmail.com>
2024-09-16 23:57:18 +0700

    update project images

--- src/data/projects.js
+++ src/data/projects.js
@@ -132,21 +132,21 @@ const projectDetailHamee = {
 const projectDetailMYNGLE = {
     singleProjectHeader: {
         singleProjectTitle: "MYNGLE",
-        singleProjectDate: "Jul 26, 2021 - Jul 26, 2022",
-        singleProjectTag: "UI / Frontend",
+        singleProjectDate: "October 2016 – March 2017",
+        singleProjectTag: "Back-end developer",
     },
     projectImages: [
         {
             id: 1,
-            img: require("@/assets/images/urala_01.png"),
+            img: require("@/assets/images/MYNGLE_01.png"),
         },
         {
             id: 2,
-            img: require("@/assets/images/urala_02.png"),
+            img: require("@/assets/images/MYNGLE_02.png"),
         },
         {
             id: 3,
-            img: require("@/assets/images/urala_03.png"),
+            img: require("@/assets/images/MYNGLE_03.png"),
         },
     ],
     projectInfo: {
@@ -155,7 +155,7 @@ const projectDetailMYNGLE = {
             {
                 id: 1,
                 title: "Name",
-                details: "Company Ltd",
+                details: "Ndex Technologies, Inc.",
             },
             {
                 id: 2,
@@ -198,21 +198,21 @@ const projectDetailMYNGLE = {
 const projectDetailCONSOLE = {
     singleProjectHeader: {
         singleProjectTitle: "CONSOLE",
-        singleProjectDate: "Jul 26, 2021 - Jul 26, 2022",
-        singleProjectTag: "UI / Frontend",
+        singleProjectDate: "July 2014 – September 2016",
+        singleProjectTag: "Back-end developer",
     },
     projectImages: [
         {
             id: 1,
-            img: require("@/assets/images/urala_01.png"),
+            img: require("@/assets/images/CONSOLE_01.png"),
         },
         {
             id: 2,
-            img: require("@/assets/images/urala_02.png"),
+            img: require("@/assets/images/CONSOLE_02.png"),
         },
         {
             id: 3,
-            img: require("@/assets/images/urala_03.png"),
+            img: require("@/assets/images/CONSOLE_03.png"),
         },
     ],
     projectInfo: {
@@ -221,7 +221,7 @@ const projectDetailCONSOLE = {
             {
                 id: 1,
                 title: "Name",
-                details: "Company Ltd",
+                details: "Zinio Vietnam",
             },
             {
                 id: 2,
@@ -264,21 +264,21 @@ const projectDetailCONSOLE = {
 const projectDetailWEBSHOP = {
     singleProjectHeader: {
         singleProjectTitle: "WEBSHOP",
-        singleProjectDate: "Jul 26, 2021 - Jul 26, 2022",
-        singleProjectTag: "UI / Frontend",
+        singleProjectDate: "December 2013 – June 2014",
+        singleProjectTag: "Back-end developer",
     },
     projectImages: [
         {
             id: 1,
-            img: require("@/assets/images/urala_01.png"),
+            img: require("@/assets/images/WEBSHOP_01.png"),
         },
         {
             id: 2,
-            img: require("@/assets/images/urala_02.png"),
+            img: require("@/assets/images/WEBSHOP_02.png"),
         },
         {
             id: 3,
-            img: require("@/assets/images/urala_03.png"),
+            img: require("@/assets/images/WEBSHOP_03.png"),
         },
     ],
     projectInfo: {
@@ -287,7 +287,7 @@ const projectDetailWEBSHOP = {
             {
                 id: 1,
                 title: "Name",
-                details: "Company Ltd",
+                details: "Lazada Vietnam",
             },
             {
                 id: 2,
@@ -328,21 +328,21 @@ const projectDetailWEBSHOP = {
 const projectDetailDATION = {
     singleProjectHeader: {
         singleProjectTitle: "DATION",
-        singleProjectDate: "Jul 26, 2021 - Jul 26, 2022",
-        singleProjectTag: "UI / Frontend",
+        singleProjectDate: "September 2012 - November 2013",
+        singleProjectTag: "Back-end & Front-end developer",
     },
     projectImages: [
         {
             id: 1,
-            img: require("@/assets/images/urala_01.png"),
+            img: require("@/assets/images/DATION_01.png"),
         },
         {
             id: 2,
-            img: require("@/assets/images/urala_02.png"),
+            img: require("@/assets/images/DATION_02.png"),
         },
         {
             id: 3,
-            img: require("@/assets/images/urala_03.png"),
+            img: require("@/assets/images/DATION_03.png"),
         },
     ],
     projectInfo: {
@@ -351,7 +351,7 @@ const projectDetailDATION = {
             {
                 id: 1,
                 title: "Name",
-                details: "Company Ltd",
+                details: "Success Software Services",
             },
             {
                 id: 2,
@@ -392,21 +392,21 @@ const projectDetailDATION = {
 const projectDetailAUTOORDER = {
     singleProjectHeader: {
         singleProjectTitle: "AUTO-ORDER (database switching)",
-        singleProjectDate: "Jul 26, 2021 - Jul 26, 2022",
-        singleProjectTag: "UI / Frontend",
+        singleProjectDate: "July 2012 - August 2012",
+        singleProjectTag: "Back-end developer",
     },
     projectImages: [
         {
             id: 1,
-            img: require("@/assets/images/urala_01.png"),
+            img: require("@/assets/images/AUTO_01.png"),
         },
         {
             id: 2,
-            img: require("@/assets/images/urala_02.png"),
+            img: require("@/assets/images/AUTO_02.png"),
         },
         {
             id: 3,
-            img: require("@/assets/images/urala_03.png"),
+            img: require("@/assets/images/AUTO_03.png"),
         },
     ],
     projectInfo: {
@@ -415,7 +415,7 @@ const projectDetailAUTOORDER = {
             {
                 id: 1,
                 title: "Name",
-                details: "Company Ltd",
+                details: "FUJINET SYSTEMS JSC",
             },
             {
                 id: 2,
@@ -453,21 +453,21 @@ const projectDetailAUTOORDER = {
 const projectDetailHYBRID = {
     singleProjectHeader: {
         singleProjectTitle: "HYBRID",
-        singleProjectDate: "Jul 26, 2021 - Jul 26, 2022",
+        singleProjectDate: "July 2011 - June 2012",
         singleProjectTag: "UI / Frontend",
     },
     projectImages: [
         {
             id: 1,
-            img: require("@/assets/images/urala_01.png"),
+            img: require("@/assets/images/HYBRID_01.png"),
         },
         {
             id: 2,
-            img: require("@/assets/images/urala_02.png"),
+            img: require("@/assets/images/HYBRID_02.png"),
         },
         {
             id: 3,
-            img: require("@/assets/images/urala_03.png"),
+            img: require("@/assets/images/HYBRID_03.png"),
         },
     ],
     projectInfo: {
@@ -476,7 +476,7 @@ const projectDetailHYBRID = {
             {
                 id: 1,
                 title: "Name",
-                details: "Company Ltd",
+                details: "FUJINET SYSTEMS JSC",
             },
             {
                 id: 2,
@@ -534,42 +534,42 @@ const projects = [
         id: 3,
         title: 'MYNGLE',
         category: 'Web Application',
-        img: require('@/assets/images/ui-project-1.jpg'),
+        img: require('@/assets/images/MYNGLE_00.jpeg'),
         detail: projectDetailMYNGLE
     },
     {
         id: 4,
         title: 'CONSOLE',
         category: 'Web Application',
-        img: require('@/assets/images/ui-project-2.jpg'),
+        img: require('@/assets/images/CONSOLE_00.jpeg'),
         detail: projectDetailCONSOLE
     },
     {
         id: 5,
         title: 'WEBSHOP',
         category: 'Web Application',
-        img: require('@/assets/images/mobile-project-1.jpg'),
+        img: require('@/assets/images/WEBSHOP_00.png'),
         detail: projectDetailWEBSHOP
     },
     {
         id: 6,
         title: 'DATION',
         category: 'Web Application',
-        img: require('@/assets/images/web-project-1.jpg'),
+        img: require('@/assets/images/DATION_00.png'),
         detail: projectDetailDATION
     },
     {
         id: 7,
         title: 'AUTO-ORDER (database switching)',
         category: 'Web Application',
-        img: require('@/assets/images/web-project-1.jpg'),
+        img: require('@/assets/images/AUTO_00.png'),
         detail: projectDetailAUTOORDER
     },
     {
         id: 8,
         title: 'HYBRID',
         category: 'Web Application',
-        img: require('@/assets/images/web-project-1.jpg'),
+        img: require('@/assets/images/HYBRID_00.jpeg'),
         detail: projectDetailHYBRID
     },
 ];
